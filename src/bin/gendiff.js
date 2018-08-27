9d8c1705cf9d20617c6792d5867b55550c47126a
Vladimir Ilin <mark09072010@gmail.com>
2018-08-27 13:16:39 +0300

    stage 1 complete

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,8 @@
 import program from 'commander';
 
 program
-  .version('1.0.0')
-  .description('cli program for compairing files')
-  .option('-f, --format [type]', 'file format')
+  .version('1.0.3')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .parse(process.argv);
