d1451d2f8e12ffc7906db187806f5f9163b63691
Vladimir Ilin <mark09072010@gmail.com>
2018-08-29 14:53:08 +0300

    fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,25 @@
 import fs from 'fs';
-import { Diff } from '../src';
+import { Diff, getObjFromFile } from '../src';
 
 describe('It works', () => {
   it('diff json', () => {
-    const first = '__tests__/__fixtures__/first.json';
-    const second = '__tests__/__fixtures__/second.json';
+    const first = getObjFromFile('__tests__/__fixtures__/first.json');
+    const second = getObjFromFile('__tests__/__fixtures__/second.json');
     expect(Diff(first, second)).toEqual(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
   });
   it('diff yaml', () => {
-    const first = '__tests__/__fixtures__/first.yaml';
-    const second = '__tests__/__fixtures__/second.yaml';
+    const first = getObjFromFile('__tests__/__fixtures__/first.yaml');
+    const second = getObjFromFile('__tests__/__fixtures__/second.yaml');
     expect(Diff(first, second)).toEqual(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
   });
   it('diff json vs yaml', () => {
-    const first = '__tests__/__fixtures__/first.json';
-    const second = '__tests__/__fixtures__/second.yaml';
+    const first = getObjFromFile('__tests__/__fixtures__/first.json');
+    const second = getObjFromFile('__tests__/__fixtures__/second.yaml');
     expect(Diff(first, second)).toEqual(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
   });
   it('diff ini', () => {
-    const first = '__tests__/__fixtures__/first.ini';
-    const second = '__tests__/__fixtures__/second.ini';
+    const first = getObjFromFile('__tests__/__fixtures__/first.ini');
+    const second = getObjFromFile('__tests__/__fixtures__/second.ini');
     expect(Diff(first, second)).toEqual(fs.readFileSync('__tests__/__fixtures__/expectedIni.txt', 'utf-8'));
   });
 });
