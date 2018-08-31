cc381e23ade000f75c7f322fe385308d9a315f93
Vladimir Ilin <mark09072010@gmail.com>
2018-08-31 23:57:52 +0300

    fixed render according recomendations

--- src/render.js
+++ src/render.js
@@ -5,33 +5,36 @@ const indent = '    ';
 const tab = num => indent.repeat(num);
 
 const strignify = (value, depth) => {
-  if (_.isObject(value)) {
-    const keys = Object.keys(value);
-    return keys.reduce((acc, item) => acc.concat(`{\n${tab(depth + 1)}  ${item}: ${value[item]}\n${tab(depth)}}`), '');
+  if (!_.isObject(value)) {
+    return value;
   }
-  return value;
+  const keys = Object.keys(value);
+  return keys.reduce((acc, item) => {
+    if (!_.isObject(item)) {
+      return acc.concat(`{\n${tab(depth + 1)}  ${item}: ${value[item]}\n${tab(depth)}}`);
+    }
+    return acc.concat(`{\n${tab(depth + 1)}  ${item}: ${strignify(item, depth + 1)}\n${tab(depth)}}`);
+  }, '');
 };
 
 const render = ast => ast.reduce((acc, item) => {
   const depth = item.level;
   const before = strignify(item.beforeValue, depth);
   const after = strignify(item.afterValue, depth);
-  if (item.type === 'nested') {
-    return acc.concat(`\n${tab(depth)}  ${item.key}: {${render(item.children)}\n${tab(depth)}}`);
+  switch (item.type) {
+    case 'nested':
+      return acc.concat(`\n${tab(depth)}  ${item.key}: {${render(item.children)}\n${tab(depth)}}`);
+    case 'unchanged':
+      return acc.concat(`\n${tab(depth)}  ${item.key}: ${before}`);
+    case 'changed':
+      return acc.concat(`\n${tab(depth)}- ${item.key}: ${before}\n${tab(depth)}+ ${item.key}: ${after}`);
+    case 'deleted':
+      return acc.concat(`\n${tab(depth)}- ${item.key}: ${before}`);
+    case 'added':
+      return acc.concat(`\n${tab(depth)}+ ${item.key}: ${after}`);
+    default:
+      return acc;
   }
-  if (item.type === 'unchanged') {
-    return acc.concat(`\n${tab(depth)}  ${item.key}: ${before}`);
-  }
-  if (item.type === 'changed') {
-    return acc.concat(`\n${tab(depth)}- ${item.key}: ${before}\n${tab(depth)}+ ${item.key}: ${after}`);
-  }
-  if (item.type === 'deleted') {
-    return acc.concat(`\n${tab(depth)}- ${item.key}: ${before}`);
-  }
-  if (item.type === 'added') {
-    return acc.concat(`\n${tab(depth)}+ ${item.key}: ${after}`);
-  }
-  return acc;
 }, '');
 
 export default render;
